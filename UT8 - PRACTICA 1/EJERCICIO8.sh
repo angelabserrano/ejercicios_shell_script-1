23a7986825a2cc45b4521b76bb05d04667ff654b
JUAN CARRILLO <alucar861@ieselcaminas.org>
2022-12-20 13:48:55 +0100

    Añadido el archivo EJERCICIO8.sh

---
+++ UT8 - PRACTICA 1/EJERCICIO8.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#!/bin/bash
+for (( i=1; i<=5; i++ ))
+do
+        for (( x=1; x<=i; x++ ))
+                do
+                        echo -e "$i\c"
+                done
+                        echo ""
+done
