2c36a271a7a903e5a07940dfb42bff756d27b354
angelabserrano <61191992+angelabserrano@users.noreply.github.com>
2023-02-06 11:56:17 +0100

    Update EJERCICIO1.sh
    Corrección del ejercicio 1. No contemplabas el caso de cuando los dos números don iguales.

--- UT8 - PRACTICA 1/EJERCICIO1.sh
+++ UT8 - PRACTICA 1/EJERCICIO1.sh
@@ -6,9 +6,11 @@ read numero1
 echo "Escribe otro numero"
 read numero2
 
-if [ $numero1 -gt $numero2 ]
-then
-	echo "$numero1 es el mayor"
+if [ $numero1 -gt $numero2 ]; then
+        echo "$numero1 es el mayor"
+elif [ $numero1 -lt $numero2 ]; then
+        echo "$numero2 es el mayor"
 else
-	echo "$numero2 es el mayor"
+        echo "Son iguales"
 fi
+
